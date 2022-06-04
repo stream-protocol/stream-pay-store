235830357cdf54298bb5ec2ee218ec354c208b68
Stream Protocol <contact@streamprotocol.org>
2022-06-04 11:27:07 +0000

    1

---
+++ .next/static/p_5DNX9eeU4_UwIbRRJgP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/431-23a9e131fa2e8a74.js","static/chunks/985-17dbb9183a6e85c3.js","static/chunks/pages/index-fba15d4e09de1a81.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
